2500511f807157d470238d90a42bf04a10dea2e1
naoki <takehara316@gmail.com>
2023-06-25 21:54:10 +0000

    finish modeling-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase } # 全てのアドレス文字を小文字にしてセーブしてくれる。
   
   validates :name,  presence: true, length: {maximum: 50 } # 名前のバリテーション
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #メールのバリテーション
-  validates :email, presence: true, length: {maximum: 100 },
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # 正規表現によるメールアドレスのフォーマット
+  validates :email, presence: true, length: {maximum: 100 },# メールのバリテーション
              format: { with: VALID_EMAIL_REGEX },
              uniqueness: true # メールアドレスの一意性
              
