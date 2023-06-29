f32754664a3d76764f9492b00d33abdddc7b76a6
naoki <takehara316@gmail.com>
2023-06-29 22:04:09 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params) # [user_params]内にはハッシュが格納されている。new.html.erbのフォーム内容（"user"=>{"name"=>"", "email"=>"", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}）
+    if @user.save
+      flash[:success] = "新規登録に成功しました。"
+      redirect_to @user # ここの@userはユーザー詳細ページへ遷移する。
+    else
+      render :new
+    end
+    
+  end
+  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation) # セキュリティー上、以下のカラムのみを許可。プライベートで情報が表に表示されない。
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#top'  #rootとはサイトのトップ画面。スタート画面などを指す。
   get '/signup', to: 'users#new'
+  resources :users # RESTfulなUsersリソースで必要となる全てのアクションも利用できる。
 end
