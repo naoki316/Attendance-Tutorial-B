f17db553e06269ffa5055b723ae47d15ed275787
naoki <takehara316@gmail.com>
2023-07-21 22:45:24 +0000

    Finish 8.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
-  def new
+  def new # ログイン機能
   end
   
-  def create
+  def create # ログイン認証機能　永続的セッションn機能
     user = User.find_by(email: params[:session][:email].downcase) # params[:session]でemailを取得。email情報をuserへ代入。
     if user && user.authenticate(params[:session][:password]) # emailを代入したuserとparams[:session]で取得したpasswordを認証。一致すればtrueを返す。
       log_in user # セッションヘルパーで定義した、ログインメソッド。このメソッドでログインが完了する。
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user # ログイン後にユーザー詳細ページへリダイレクトする。
+      redirect_back_or user # ログイン後に以前のページへリダイレクトする。フレンドリーフォワーディディング機能。
     else
      flash.now[:danger] = "認証に失敗しました。"
      render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
   
   def new
     @user = User.new
   end
   
   def show
-    @user = User.find(params[:id])
   end
   
   def create
@@ -21,11 +27,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -38,6 +42,27 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation) # セキュリティー上、以下のカラムのみを許可。プライベートで情報が表に表示されない。
+    end
+    
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
     end
   
 end
